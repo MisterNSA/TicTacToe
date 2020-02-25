59718046bbf6156e67d0f1b11aa8713e570c5ed9
MisterNSA <53508377+MisterNSA@users.noreply.github.com>
2020-02-25 15:00:26 +0100

    Nun mit Singleplayer

--- TicTest.py
+++ TicTest.py
@@ -1,17 +1,19 @@
 #TicTacToe
-#Geplante Features: Singleplayer vs Bot. / Online Multiplayer? / GUI?
+#Geplante Features: Singleplayer vs Bot(Erledigt) / Online Multiplayer? / GUI?
 #Ersteller: Tobias Dominik Weber aka. MisterNSA 
-#Version: 0.8 Datum: 25.02.2020
+#Version: 1.0 Datum: 25.02.2020
 
 import TicTacToe
 
 def Menu():
 
-    Menu_item = int(input("Do you want to play an new multiplayer game (1) or read through the rules (2)?"))
+    Menu_item = int(input("Do you want to play an new multiplayer game (1), play singleplayer (2) or read through the rules (3)?"))
 
     if Menu_item == 1:
         TicTacToe.Multiplayer_Game()
     elif Menu_item == 2:
+        TicTacToe.Singleplayer_Game()
+    elif Menu_item == 3:
         TicTacToe.Rules()
         Menu()
     else:
@@ -19,3 +21,4 @@ def Menu():
         Menu()
 
 Menu()
+
