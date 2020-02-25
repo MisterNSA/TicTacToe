32d169033336cf9d441aec73457e972bb699f338
MisterNSA <53508377+MisterNSA@users.noreply.github.com>
2020-02-25 10:23:07 +0100

    Bugfixes and updated graphics
    -Fixed a Bug where inputing anything but a number caused the game to crash
    -Fixed a Bug where the player_move would loop and output Referenced before assigned errors
    
    updated the graphics to be more Symmetric

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,37 +1,48 @@
 #Funktionsbibliothek für TicTacToe
 #Ersteller: Tobias Dominik Weber aka. MisterNSA 
-#Version: 0.7 Datum: 23.02.2020
+#Version: 0.8 Datum: 25.02.2020
 
 
 #Zeigt das Feld an
 def drawBoard(board):                                                    
-    print(board[1] + '|' + board[2] + '|' + board[3])
-    print('-+-+-')
-    print(board[4] + '|' + board[5] + '|' + board[6])
-    print('-+-+-')
-    print(board[7] + '|' + board[8] + '|' + board[9])
+    print(" " + board[1] + " " + "|" + " " + board[2] + " " +  "|" + " " + board[3] + " ")
+    print('---+---+---')
+    print(" " + board[4] + " " + "|" + " " + board[5] + " " +  "|" + " " + board[6] + " ")
+    print('---+---+---')
+    print(" " + board[7] + " " + "|" + " " + board[8] + " " +  "|" + " " + board[9] + " ")
 
 
 #Nimmt Input entgegen, prüft ob Zug möglich ist und führt diesen aus
 def player_move(board, sign, Player):
     while True:
-        """PROBLEM: WENN FIELD KEIN INT -> CRASH!!!"""
-        field = int(input("On which field do you want to place a {} {}? 1-9: ".format(sign, Player)))  #Auswählen auf welchem Feld das Zeichen gesetzt werden soll
-        if field < 1 or field > 9:      #Wenn Feld auserhalb der Reichweite
+        try:    #try, da field mit einem nicht int konformen Wert zu einem Absturtz führt
+            field = int(input("On which field do you want to place a {} {}? 1-9: ".format(sign, Player)))  #Auswählen auf welchem Feld das Zeichen gesetzt werden soll
+        except ValueError: #Wenn field kein Int
+            print("The field must be a Number between 1 and 9!")
+            player_move(board, sign, Player)
+            break #Springt nach draw Board hierhin zurück. Ohne break gibt es einen Referenced before assigned error bei "if field < 1 or field > 9:""
+        
+        if field < 1 or field > 9:      #Wenn Feld auserhalb der Reichweite  
             print("Please Input a Number between 1 and 9 to choose your field. 1 for the top left and 9 for the bottom right.")
         elif board[field] != " ":       #Wenn schon ein Wert im Feld steht
             print("Theres already a {} in this Field. Please choose another one.".format(sign))
         else:
-            break   
-    board[field] = sign #In Liste board momentanes Feld mit Zeichen beschreiben
-    drawBoard(board)
+            pass   
+        board[field] = sign #In Liste board momentanes Feld mit Zeichen beschreiben
+        drawBoard(board)
+        break #Springt immer zum except zurück - WARUM?
 
 
 #Shows the rules of the game
 def Rules():  
     print("The Goal of this Game is to be the first Player with three Symbols in a row.")
     print("Rows count horizontal, vertical and diagonal.")
-    print("When the Game asks you to input a Number to choose a field and confirm with the Enter-Key. The fields are addressed with the following numbers:\n 1 | 2 | 3\n-----------\n 4 | 5 | 6\n-----------\n 7 | 8 | 9")
+    print("When the Game asks you to input a Number to choose a field and confirm with the Enter-Key. The fields are addressed with the following numbers:\n")
+    print(" " + "1" + " " + "|" + " " + "2" + " " +  "|" + " " + "3" + " ")
+    print('---+---+---')
+    print(" " + "4" + " " + "|" + " " + "5" + " " +  "|" + " " + "6" + " ")
+    print('---+---+---')
+    print(" " + "7" + " " + "|" + " " + "8" + " " +  "|" + " " + "9" + " \n")
 
 #Überprüft, ob Jemand das Spiel gewonnen hat (True), alle Felder belegt wurden ohne Sieger("Draw"), oder es noch Zugmöglichkeiten gibt(False). 
 """Erstmal alle Möglichkeiten durchzutesten / Später Neuronales Netz?"""
@@ -65,7 +76,6 @@ def Win(board, Turn_Count):
             return ("Draw")
 
     
-
 #The Multiplayer Game
 def Multiplayer_Game():
             board = [" "," "," "," "," "," "," "," "," "," "]
